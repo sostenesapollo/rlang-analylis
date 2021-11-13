7443b217471266bce9e71f1247e3d66d422934fa
Sostenes Apollo <sostenesapollo25@gmail.com>
2021-11-13 15:11:15 -0300

    feat: cleaning DT_ENCERRAMENTO with null value

--- csv_analysis.r
+++ csv_analysis.r
@@ -1,6 +1,30 @@
+# Need to install the packages via command line
+# install.packages("lubridate")
+# install.packages("tidyr")
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
 data = read.csv('INFLUD19-16042020.csv', header=TRUE, sep=';')
-dates = data.frame(
-    encerramento = data$DT_ENCERRAMENTO,
-    notificacao = data$DT_NOTIFICACAO
-)
+
+# Remove all the rows that don't have a value for DT_ENCERRAMENTO
+data <- data[data$DT_ENCERRAMENTO != '',]
+# Get just the DT_NOTIFICACAO and DT_ENCERRAMENTO in a new dataframe
+dates <- select(data, DT_NOTIFICACAO, DT_ENCERRAMENTO)
+
+# data$notificacao  <- as.Date(data$notificacao, format = '%d/%m/%Y')
+# data$encerramento <- as.Date(data$encerramento, format = '%d/%m/%Y')
+
+
+# datas <- data %>% 
+#   drop_na(DT_ENCERRAMENTO)
+
+# data <- data %>% filter(DT_ENCERRAMENTO == "")
+
+# df = data.frame(notificacao= as.Date(data$DT_NOTIFICACAO, format = '%d/%m/%Y'), encerramento= as.Date(data$DT_ENCERRAMENTO, format = '%d/%m/%Y'))
+
+# ## Agrupando e calculando a diferença entre as datas com o dplyr
+# mutate(df, diferenca = encerramento - notificacao)
+
+print(dates)
 View(dates)
