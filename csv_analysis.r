34d6fd05346866b822ccee75beeb7d4baffaa82e
Sostenes Apollo <sostenesapollo25@gmail.com>
2021-11-13 15:28:13 -0300

    feat: generate a new column that gives the dates difference in days

--- csv_analysis.r
+++ csv_analysis.r
@@ -2,8 +2,6 @@
 # install.packages("lubridate")
 # install.packages("tidyr")
 library(dplyr)
-library(tidyr)
-library(lubridate)
 
 data = read.csv('INFLUD19-16042020.csv', header=TRUE, sep=';')
 
@@ -12,19 +10,12 @@ data <- data[data$DT_ENCERRAMENTO != '',]
 # Get just the DT_NOTIFICACAO and DT_ENCERRAMENTO in a new dataframe
 dates <- select(data, DT_NOTIFICACAO, DT_ENCERRAMENTO)
 
-# data$notificacao  <- as.Date(data$notificacao, format = '%d/%m/%Y')
-# data$encerramento <- as.Date(data$encerramento, format = '%d/%m/%Y')
+dates <- data.frame(
+    notificacao= as.Date(dates$DT_NOTIFICACAO, format = '%d/%m/%Y'),
+    encerramento= as.Date(dates$DT_ENCERRAMENTO, format = '%d/%m/%Y')
+)
 
-
-# datas <- data %>% 
-#   drop_na(DT_ENCERRAMENTO)
-
-# data <- data %>% filter(DT_ENCERRAMENTO == "")
-
-# df = data.frame(notificacao= as.Date(data$DT_NOTIFICACAO, format = '%d/%m/%Y'), encerramento= as.Date(data$DT_ENCERRAMENTO, format = '%d/%m/%Y'))
-
-# ## Agrupando e calculando a diferença entre as datas com o dplyr
-# mutate(df, diferenca = encerramento - notificacao)
+dates$diferenca_dias <- as.integer(dates$encerramento - dates$notificacao)
 
 print(dates)
 View(dates)
