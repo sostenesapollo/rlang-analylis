49af9cd834663c7b52debd040062860f75ac6537
Sostenes Apollo <sostenesapollo25@gmail.com>
2021-11-16 12:02:27 -0300

    fix: update MA string

--- 5.unidade_mais_casos_ma.r
+++ 5.unidade_mais_casos_ma.r
@@ -1,14 +1,16 @@
 library(dplyr)
 
+par(mfrow = c(1, 1))
+
 data = read.csv('INFLUD19-16042020.csv', header=TRUE, sep=';')
-df <- filter(data, SG_UF_NOT == 'PI')
+df <- filter(data, SG_UF_NOT == 'MA')
 tb <- table(df$ID_UNIDADE)
 
 #sort tb by value
 tb <- tb[order(tb)]
 
 unidades_com_mais_casos <- tail(tb, 7)
-pie(unidades_com_mais_casos, col=rainbow(7), cex=0.7, main="Unidades com mais casos no PI")
+pie(unidades_com_mais_casos, col=rainbow(7), cex=0.7, main="Unidades com mais casos no MA")
 
 # # Média, max, min, Mediana
 print('Resumo')
