ebeb671a91e7e531a81cd89931d3e5cefebee34e
Sostenes Apollo <sostenesapollo25@gmail.com>
2021-11-15 21:39:18 -0300

    feat: filter if code region greater than 1497

---
+++ test.r
@@ -0,0 +1,8 @@
+# id = c(1,1,2,2,3)
+# nome = c("a","b","c","d","e")
+# salario = c(8000,2000,3000, 6000,5000)
+
+# df = data.frame(id,nome,salario)
+# # View(df[1:2,])
+
+# barplot(table(cut(df$id, seq(1,3))))
